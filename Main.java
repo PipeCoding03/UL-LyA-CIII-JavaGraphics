44d470b06bdef7feb4ae77c5724225703e9bc03b
Andrés Martínez <AFMV3007@gmail.com>
2023-05-29 10:27:44 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
+import javax.swing.JFrame;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Prueba");
+        int WIDTH = 800;
+        int HEIGHT = 450;
+
+        JFrame window = new JFrame("Java Graphics");
+        window.setSize(WIDTH, HEIGHT);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
