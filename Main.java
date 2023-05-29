5ae7b4c4cbf3fddc34781fa7910c878826d7fc72
Andrés Martínez <AFMV3007@gmail.com>
2023-05-29 10:37:17 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,20 @@
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Main {
     public static void main(String[] args) {
         int WIDTH = 800;
         int HEIGHT = 450;
-
         JFrame window = new JFrame("Java Graphics");
+        JPanel panel = new JPanel();
+
         window.setSize(WIDTH, HEIGHT);
         window.setLocationRelativeTo(null);
         window.setVisible(true);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel.setBackground(new Color(247, 68, 68));
+        window.add(panel);
     }
 }
